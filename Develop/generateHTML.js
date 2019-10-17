7caa5b3fb17cb7804ea1c2b92d7d2b4d0fc221bf
Polar Bear <bballare21@gmail.com>
2019-10-16 19:56:17 -0700

    updated files

---
+++ Develop/generateHTML.js
@@ -0,0 +1,35 @@
+function generateHTML(data) {
+    console.log(data);
+    return `<!DOCTYPE html>
+  <html lang="en">
+     <head>
+        <meta charset="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta http-equiv="X-UA-Compatible" content="ie=edge" />
+        <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css"/>
+        <link href="https://fonts.googleapis.com/css?family=BioRhyme|Cabin&display=swap" rel="stylesheet">
+        <title>Document</title>
+        <style>
+            @page {
+              margin: 0;
+            }
+           *,
+           *::after,
+           *::before {
+           box-sizing: border-box;
+           }
+           html, body {
+           padding: 0;
+           margin: 0;
+           }
+           
+        </style>
+        </head>
+        <body>
+              
+        </body>
+  
+        </html>`
+          }
+  
+  module.exports = generateHTML;
